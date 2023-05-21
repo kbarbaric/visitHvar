1a914b1c8a9f6040fcfa3f2c301a3167017e32df
kbarbaric <115696047+kbarbaric@users.noreply.github.com>
2023-05-21 12:20:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const Hvala=()=>{
+    alert("Zahvaljujemo se na recenziji!");
+    document.getElementsByClassName('fa-solid fa-umbrella-beach').style.display='auto';
+    document.body.getElementsByClassName('fa-solid fa-umbrella-beach').backgroundColor = "red";
+}
+
+const PromijeniBoju=()=>{
+    document.getElementById('srce1').style.color="red";
+}
+const PromijeniBoju2=()=>{
+    document.getElementById('srce2').style.color="red";
+}
+const PromijeniBoju3=()=>{
+    document.getElementById('srce3').style.color="red";
+}
+const PromijeniBoju4=()=>{
+    document.getElementById('srce4').style.color="red";
+}
